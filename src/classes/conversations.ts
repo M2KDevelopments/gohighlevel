cf296faaea8292b0b478ad6f35ee4eacc01d2ee0
M2K Developments <m2ksoftiez@gmail.com>
2024-11-21 15:04:30 +0200

    Quick implementation of adding Inbound Message to conversation

--- src/classes/conversations.ts
+++ src/classes/conversations.ts
@@ -28,7 +28,6 @@ interface ISearchConversation {
     total: number,
     traceId: string
 }
-
 interface IConversateMessages {
     messages: {
         lastMessageId?: string,
@@ -39,6 +38,44 @@ interface IConversateMessages {
     traceId: string
 }
 
+/**
+ * https://highlevel.stoplight.io/docs/integrations/3c9036411fcc3-add-an-inbound-message
+ */
+interface InboundMessage {
+    type: "SMS" | "Email" | "WhatsApp" | "GMB" | "IG" | "FB" | "Custom" | "WebChat" | "Live_Chat" | "Call",
+
+    /** Array of urls */
+    attachments?: string[],
+    message: string,
+    conversationId: string,
+
+    /** Conversation Providers are created in the GHL Market Place you can find the IDS there */
+    conversationProviderId: string,
+    direction: "inbound" | "outbound",
+
+    /** Date format e.g 2024-11-20T14:15:22Z*/
+    date: string,
+
+    html?: string,
+    subject?: string,
+    emailFrom?: string,
+    emailTo?: string,
+    emailCc?: string[],
+    emailBcc?: string[],
+    emailMessageId?: string,
+    call: {
+        to: string,
+        from: string,
+        status: "ending" | "completed" | "answered" | "busy" | "no-answer" | "failed" | "canceled" | "voicemail"
+
+    }
+}
+interface InboundSuccess {
+    success: boolean,
+    conversationId: string,
+    messageId: string,
+    traceId: string
+}
 
 export class Conversations {
     private authData?: AuthData;
@@ -82,6 +119,19 @@ export class Conversations {
         }
         const response = await axios.get(`https://services.leadconnectorhq.com/conversations/${conversationId}/messages`, { headers });
         const c: IConversateMessages = response.data;
+        return c;
+    }
+
+
+    async addInboundMessage(props: InboundMessage) {
+        const access_token = this.authData?.access_token;
+        const headers = {
+            "Version": "2021-04-15",
+            "Authorization": "Bearer " + access_token,
+            "Accept": 'application/json'
+        }
+        const response = await axios.post(`https://services.leadconnectorhq.com/conversations/messages/inbound`, props, { headers });
+        const c: InboundSuccess = response.data;
         return c;
     }
 
