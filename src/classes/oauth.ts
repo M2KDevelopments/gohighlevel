82fbffff789c603f607f8b002eda99b782f48971
M2K Developments <m2ksoftiez@gmail.com>
2024-12-11 17:04:02 +0200

    Add all scopes

--- src/classes/oauth.ts
+++ src/classes/oauth.ts
@@ -89,6 +89,45 @@ export class OAuth {
         return this;
     }
 
+    /**
+     * Add all scopes
+     */
+    scopeAll() {
+        this.credientials.scopes = [
+            'companies.readonly',
+            'conversations.readonly',
+            'links.write',
+            'links.readonly',
+
+            'calendars.write',
+            'calendars.readonly',
+            'calendars/events.readonly',
+            'calendars/events.write',
+
+            'calendars/groups.readonly',
+            'calendars/groups.write',
+
+            'calendars/resources.readonly',
+            'calendars/resources.write',
+
+            'locations.write',
+            'locations/customFields.readonly',
+            'locations/customValues.write',
+            'locations/customFields.write',
+            'locations/customValues.readonly',
+            'locations/tags.write',
+            'locations/tags.readonly',
+            'locations.readonly',
+            'opportunities.readonly',
+            'opportunities.write',
+            'businesses.readonly',
+            'businesses.write',
+            'contacts.readonly',
+            'contacts.write',
+            'locations/tasks.readonly',
+            'locations/tasks.write'];
+    }
+
 
     // Blogs
     scopeBlogsPostReadonly() { return this.addScope("blogs/post.write"); }
